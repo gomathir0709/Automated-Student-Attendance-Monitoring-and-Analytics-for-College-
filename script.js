3149b3fc6d38577d554cbbd2ae661f7ffb92baa5
gomathir0709 <gomathir0709@gmail.com>
2026-07-04 16:25:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,28 @@ function login() {
         alert("Invalid Username or Password!");
     }
 }
+function addStudent() {
+    let roll = document.getElementById("roll").value;
+    let name = document.getElementById("name").value;
+    let department = document.getElementById("department").value;
+    let year = document.getElementById("year").value;
+
+    let students = JSON.parse(localStorage.getItem("students")) || [];
+
+    students.push({
+        roll: roll,
+        name: name,
+        department: department,
+        year: year
+    });
+
+    localStorage.setItem("students", JSON.stringify(students));
+
+    alert("Student Added Successfully!");
+
+    document.getElementById("roll").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("department").value = "";
+    document.getElementById("year").value = "";
+}
 
